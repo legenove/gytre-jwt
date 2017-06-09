c4f21df186967ba624ab72ed71d51bbf93cc9ecf
liuguoyang <396410414@qq.com>
2017-06-09 17:49:45 +0800

    update jwt some function in util

--- gytre/jwt.lua
+++ gytre/jwt.lua
@@ -7,11 +7,9 @@ local ngx = ngx
 local os_time = os.time
 local ngx_b64_encode = ngx.encode_base64
 local ngx_b64_decode = ngx.decode_base64
-local table_insert = table.insert
-local table_sort = table.sort
 local table_concat = table.concat
 local error = error
-local pairs = pairs
+local pcall = pcall
 local setmetatable = setmetatable
 local type = type
 
@@ -22,15 +20,6 @@ local _M = {
     RSA_PRIV_KEY = nil,
     SECRET_KEY = nil,
 }
-
-local function table_dict_sort(dict)
-    local keys = {}
-    local res = {}
-    for i in pairs(dict) do table_insert(keys, i) end
-    table_sort(keys)
-    for i,v in pairs(keys) do res[v]=dict[v] end
-    return res
-end
 
 local function rsa_encode(algorithm, data, pub_key)
     local pub, err = rsa:new({
@@ -123,32 +112,6 @@ local preload = {
     jti=""
 }
 
---lua table 拷贝
-local function table_copy_table(ori_tab)
-    if (type(ori_tab) ~= "table") then
-        return nil
-    end
-    local new_tab = {}
-    for i,v in pairs(ori_tab) do
-        local vtyp = type(v)
-        if (vtyp == "table") then
-            new_tab[i] = table_copy_table(v)
-        elseif (vtyp == "thread") then
-            new_tab[i] = v
-        elseif (vtyp == "userdata") then
-            new_tab[i] = v
-        else
-            new_tab[i] = v
-        end
-    end
-    return new_tab
-end
-
-local function table_merge( tDest, tSrc )
-    for k, v in pairs( tSrc ) do
-        tDest[k] = v
-    end
-end
 
 local function gen_jwt_id()
     return ngx_b64_encode(3193174198+os_time())
@@ -167,29 +130,17 @@ function _M.create_preload(o)
     o.nbf = o.nbf or (os_time())
     o.iat = o.nbf or (os_time())
     o.jti = o.jti or (gen_jwt_id())
-    local _preload = table_copy_table(preload)
-    table_merge(_preload,o)
+    local _preload = util:table_copy_table(preload)
+    util.table_merge(_preload,o)
     return _preload
 end
 
-local startswith = function(str, substr)
-    if str == nil or substr == nil then
-        return nil, "the string or the sub-stirng parameter is nil"
-    end
-    if string.find(str, substr) ~= 1 then
-        return false
-    else
-        return true
-    end
-end
-
-
 function _M:encode(data, alg)
 	if type(data) ~= 'table' then return nil, "Argument #1 must be table" end
-    data = table_dict_sort(data)
+    data = util.table_dict_sort(data)
     local key
 	alg = alg or "HS256"
-    if startswith(alg,'HS') then
+    if util.startswith(alg,'HS') then
         key = self.SECRET_KEY
     else
         key = self.RSA_PUB_KEY
@@ -255,7 +206,7 @@ function _M:decode(data, verify)
 		if body.nbf and type(body.nbf) ~= "number" then
 			return nil, "nbf must be number"
         end
-        if startswith(header.alg, 'HS') then
+        if util.startswith(header.alg, 'HS') then
             key = self.SECRET_KEY
         else
             key = self.RSA_PRIV_KEY
--- gytre/util.lua
+++ gytre/util.lua
@@ -2,7 +2,12 @@ local string_gsub = string.gsub
 local string_format = string.format
 local string_byte = string.byte
 local string_char = string.char
+local string_find = string.find
+local table_insert = table.insert
+local table_sort = table.sort
+local pairs = pairs
 local tonumber = tonumber
+local type = type
 
 local _M = { _VERSION = '0.01' }
 
@@ -14,6 +19,53 @@ end
 _M.encodeURI = function(s)
     s = string_gsub(s, "([^%w%.%- ])", function(c) return string_format("%%%02X", string_byte(c)) end)
     return string_gsub(s, " ", "+")
+end
+
+
+_M.startswith = function(str, substr)
+    if str == nil or substr == nil then
+        return nil, "the string or the sub-stirng parameter is nil"
+    end
+    if string_find(str, substr) ~= 1 then
+        return false
+    else
+        return true
+    end
+end
+
+_M.table_dict_sort = function(dict)
+    local keys = {}
+    local res = {}
+    for i in pairs(dict) do table_insert(keys, i) end
+    table_sort(keys)
+    for i,v in pairs(keys) do res[v]=dict[v] end
+    return res
+end
+
+_M.table_copy_table = function(self, ori_tab)
+    if (type(ori_tab) ~= "table") then
+        return nil
+    end
+    local new_tab = {}
+    for i,v in pairs(ori_tab) do
+        local vtyp = type(v)
+        if (vtyp == "table") then
+            new_tab[i] = self.table_copy_table(v)
+        elseif (vtyp == "thread") then
+            new_tab[i] = v
+        elseif (vtyp == "userdata") then
+            new_tab[i] = v
+        else
+            new_tab[i] = v
+        end
+    end
+    return new_tab
+end
+
+_M.table_merge = function( tDest, tSrc )
+    for k, v in pairs( tSrc ) do
+        tDest[k] = v
+    end
 end
 
 return _M
